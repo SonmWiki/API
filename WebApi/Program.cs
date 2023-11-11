f96d99af33c1f61b0920cc80803d0179e16b5244
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-11-11 16:42:55 +0300

    feat(webapi): added Infratrtucture services to webapi

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,9 @@
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
