d797f3291d1a8e3220d681ce9c482e40301b5e86
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-11-19 10:44:44 +0300

    feat(infrastructure): initial database migrations

---
+++ Infrastructure/Migrations/20231111123848_Initial.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    Weight = table.Column<int>(type: "integer", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Path = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    IsVisible = table.Column<bool>(type: "boolean", nullable: false),
+                    Weight = table.Column<int>(type: "integer", nullable: false, defaultValue: 0),
+                    Categoryid = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Articles_Categories_Categoryid",
+                        column: x => x.Categoryid,
+                        principalTable: "Categories",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Revisions",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "text", nullable: false),
+                    Articleid = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Author = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Reviewer = table.Column<string>(type: "text", nullable: false),
+                    Review = table.Column<string>(type: "text", nullable: true),
+                    ReviewTimestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Revisions", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Revisions_Articles_Articleid",
+                        column: x => x.Articleid,
+                        principalTable: "Articles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Categoryid",
+                table: "Articles",
+                column: "Categoryid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Path",
+                table: "Articles",
+                column: "Path",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Revisions_Articleid",
+                table: "Revisions",
+                column: "Articleid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Revisions");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20231117195355_TableRelationFix.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TableRelationFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Revisions_Articles_Articleid",
+                table: "Revisions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Articleid",
+                table: "Revisions",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Revisions_Articles_Articleid",
+                table: "Revisions",
+                column: "Articleid",
+                principalTable: "Articles",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Revisions_Articles_Articleid",
+                table: "Revisions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Articleid",
+                table: "Revisions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Revisions_Articles_Articleid",
+                table: "Revisions",
+                column: "Articleid",
+                principalTable: "Articles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
