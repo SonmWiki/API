a64a261e8592f914cae2dee577e95d6f6f1d1663
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-04 00:35:30 +0300

    feat(webapi): automatic entity framework migrations

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Application;
 using Application.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,20 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.EnsureCreated();
+    try
+    {
+        dbContext.Database.EnsureCreated();
+        await dbContext.Database.MigrateAsync();
+    }
+    catch (Exception e)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        
+        logger.LogError(e, "There was an error while creating or migrating database");
+        
+        Console.WriteLine(e);
+        throw;
+    }
 }
 
 app.MapGet("/", () => "Hello World!");
