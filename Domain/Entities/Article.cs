94c494706b23cc339e73508a070feb762787aef8
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-08 02:13:33 +0300

    feat(domain): added Title property to Article

--- Domain/Entities/Article.cs
+++ Domain/Entities/Article.cs
@@ -4,6 +4,7 @@ namespace Domain.Entities;
 
 public class Article : BaseEntity<string>
 {
+    public required string Title { get; set; }
     public bool IsVisible { get; set; }
     public string? RedirectArticleId { get; set; }
     public virtual Article? RedirectArticle { get; set; }
