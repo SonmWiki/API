187cfc355b1f1c156fbf47e688d94dbe459a717f
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-08 02:12:52 +0300

    feat(domain): added field Name to Category

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -4,6 +4,7 @@ namespace Domain.Entities;
 
 public class Category : BaseEntity<string>
 {
+    public required string Name { get; set; }
     public string? ParentId { get; set; }
     public virtual Category? Parent { get; set; }
     public virtual ICollection<Category> SubCategories { get; set; } = new List<Category>();
