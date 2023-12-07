8d6f6ccaf4f9a737c372800003566c0eae24001d
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-08 02:18:03 +0300

    updated errors to be consistent with addition of Name and Title field of Category and Article in Domain

--- Application/Features/Articles/Errors/Errors.cs
+++ Application/Features/Articles/Errors/Errors.cs
@@ -6,11 +6,16 @@ public static class Article
 {
     public static readonly Error NotFound = Error.NotFound(
         code: "Article.NotFound",
-        description: "Article with this title was not found."
+        description: "Article with this ID was not found."
     );
 
-    public static readonly Error DuplicateTitle = Error.Conflict(
-        code: "Article.DuplicateTitle",
-        description: "Article with this title already exists."
+    public static readonly Error DuplicateId = Error.Conflict(
+        code: "Article.DuplicateId",
+        description: "Article with similar title that results in same ID already exists."
+    );
+
+    public static readonly Error EmptyId = Error.Validation(
+        code: "Article.EmptyId",
+        description: "Generated ID for this article is empty."
     );
 }
--- Application/Features/Categories/Errors/Errors.cs
+++ Application/Features/Categories/Errors/Errors.cs
@@ -6,16 +6,21 @@ public static class Category
 {
     public static Error NotFound = Error.NotFound(
         code: "Category.NotFound",
-        description: "Category with this name was not found."
+        description: "Category with this ID was not found."
     );
 
     public static Error ParentNotFound = Error.NotFound(
         code: "Category.ParentNotFound",
-        description: "Category with this name was not found."
+        description: "Category with this parent Id was not found."
     );
 
-    public static Error DuplicateName = Error.Conflict(
-        code: "Category.DuplicateName",
-        description: "Category with this name already exists."
+    public static Error DuplicateId = Error.Conflict(
+        code: "Category.DuplicateId",
+        description: "Category with similar name that results in same ID already exists."
+    );
+    
+    public static readonly Error EmptyId = Error.Validation(
+        code: "Category.EmptyId",
+        description: "Generated ID for this category is empty."
     );
 }
