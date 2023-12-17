e633ee5ae523e146820645d7cb83b3e3f199a900
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-17 03:45:52 +0300

    feat(infrastructure): create or update author on keycloak user authentication

--- Infrastructure/Extensions/ServiceCollectionExt.cs
+++ Infrastructure/Extensions/ServiceCollectionExt.cs
@@ -1,6 +1,7 @@
 ﻿using System.Security.Claims;
 using Application.Authorization.Abstractions;
 using Application.Data;
+using Domain.Entities;
 using Infrastructure.Authorization;
 using Infrastructure.Data;
 using Keycloak.AuthServices.Authentication;
@@ -21,8 +22,31 @@ public static class ServiceCollectionExt
 
         services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
             options.UseNpgsql(connectionString));
-        
-        services.AddKeycloakAuthentication(configuration);
+
+        services.AddKeycloakAuthentication(configuration, options =>
+        {
+            options.Events ??= new JwtBearerEvents();
+
+            options.Events.OnTokenValidated += async context =>
+            {
+                var principal = context.Principal;
+                var id = principal?.FindFirst(ClaimTypes.NameIdentifier);
+                if (id == null) return;
+                if (principal?.Identity?.Name == null) return;
+                if (principal.Identity.IsAuthenticated == false) return;
+
+                var dbContext = context.HttpContext.RequestServices.GetService<IApplicationDbContext>();
+
+                var author = new Author {Id = id.Value, Name = principal.Identity.Name};
+                var exists = dbContext.Authors.Any(e => e.Id == id.Value);
+                if (exists)
+                    dbContext.Authors.Update(author);
+                else
+                    dbContext.Authors.Add(author);
+
+                await dbContext.SaveChangesAsync();
+            };
+        });
         services.AddAuthorization();
         services.AddKeycloakAuthorization(configuration);
 
