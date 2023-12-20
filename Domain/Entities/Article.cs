5ceb62f629b1627dcd1842804abbfaf22b1b116d
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-20 23:56:47 +0300

    feat(domain): changed Article IsVisible property to ArticleStatus enum property

--- Domain/Entities/Article.cs
+++ Domain/Entities/Article.cs
@@ -5,10 +5,18 @@ namespace Domain.Entities;
 public class Article : BaseEntity<string>
 {
     public required string Title { get; set; }
-    public bool IsVisible { get; set; }
+    public ArticleStatus ArticleStatus { get; set; }
     public string? RedirectArticleId { get; set; }
     public virtual Article? RedirectArticle { get; set; }
     public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
     public virtual ICollection<ArticleCategory> ArticleCategories { get; set; } = new List<ArticleCategory>();
     public virtual ICollection<Revision> Revisions { get; set; } = new List<Revision>();
+}
+
+public enum ArticleStatus
+{
+    Submitted,
+    Rejected,
+    Redirect,
+    Active
 }
