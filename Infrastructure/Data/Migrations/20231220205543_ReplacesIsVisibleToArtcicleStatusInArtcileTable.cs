a36379f53c59edb429c0e0aab1cc289c376fadb6
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-20 23:57:23 +0300

    feat(infrastructure): migration for ArticleStatus

---
+++ Infrastructure/Data/Migrations/20231220205543_ReplacesIsVisibleToArtcicleStatusInArtcileTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplacesIsVisibleToArtcicleStatusInArtcileTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVisible",
+                table: "Articles");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ArticleStatus",
+                table: "Articles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArticleStatus",
+                table: "Articles");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVisible",
+                table: "Articles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
