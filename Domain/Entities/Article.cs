fa35b8a9ca526e70cd3618c6804b1e32b7400763
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-22 17:47:04 +0300

    feat(domain): changed Article.ArticleStatus property to IsHidden

--- Domain/Entities/Article.cs
+++ Domain/Entities/Article.cs
@@ -5,18 +5,10 @@ namespace Domain.Entities;
 public class Article : BaseEntity<string>
 {
     public required string Title { get; set; }
-    public ArticleStatus ArticleStatus { get; set; }
+    public bool IsHidden { get; set; }
     public string? RedirectArticleId { get; set; }
     public virtual Article? RedirectArticle { get; set; }
     public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
     public virtual ICollection<ArticleCategory> ArticleCategories { get; set; } = new List<ArticleCategory>();
     public virtual ICollection<Revision> Revisions { get; set; } = new List<Revision>();
-}
-
-public enum ArticleStatus
-{
-    Submitted,
-    Rejected,
-    Redirect,
-    Active
 }
