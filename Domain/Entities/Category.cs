618f63b7d74877d129aa7791bc1e92fad1163673
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-12-22 20:23:49 +0300

    feat(domain): missing navigation property Revisions in Category

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -10,4 +10,5 @@ public class Category : BaseEntity<string>
     public virtual ICollection<Category> SubCategories { get; set; } = new List<Category>();
     public virtual ICollection<Article> Articles { get; set; } = new List<Article>();
     public virtual ICollection<ArticleCategory> CategoryArticles { get; set; } = new List<ArticleCategory>();
+    public virtual ICollection<Revision> Revisions { get; set; } = new List<Revision>();
 }
