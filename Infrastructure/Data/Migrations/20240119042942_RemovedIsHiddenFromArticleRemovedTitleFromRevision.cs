71be6b379ac528341c5d7407f8068ca8a792889c
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-01-19 07:31:45 +0300

    feat(infrastructure): migration to keep up with domain changes

---
+++ Infrastructure/Data/Migrations/20240119042942_RemovedIsHiddenFromArticleRemovedTitleFromRevision.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedIsHiddenFromArticleRemovedTitleFromRevision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Revisions");
+
+            migrationBuilder.DropColumn(
+                name: "IsHidden",
+                table: "Articles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Revisions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHidden",
+                table: "Articles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
