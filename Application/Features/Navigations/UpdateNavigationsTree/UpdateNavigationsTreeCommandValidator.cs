b16ff40af8379edc2037b8a8360540847fd818f7
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-01-28 22:12:44 +0300

    feat(application): UpdateNavigationsTreeCommandValidator

---
+++ Application/Features/Navigations/UpdateNavigationsTree/UpdateNavigationsTreeCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Navigations.UpdateNavigationsTree;
+
+public class UpdateNavigationsTreeCommandValidator : AbstractValidator<UpdateNavigationsTreeCommand>
+{
+    public UpdateNavigationsTreeCommandValidator()
+    {
+        RuleForEach(v => v.Data).SetValidator(new UpdateNavigationsTreeCommandElementValidator());
+    }
+}
+
+public class UpdateNavigationsTreeCommandElementValidator : AbstractValidator<UpdateNavigationsTreeCommand.Element>
+{
+    public UpdateNavigationsTreeCommandElementValidator()
+    {
+        RuleFor(v => v.Name).NotEmpty();
+        RuleFor(v => v.Children).ForEach(v=>v.SetValidator(this));
+    }
+}
