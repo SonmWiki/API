c12cf964f6c8a9be9d34e7c948e2162cbf77e972
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2024-02-07 16:37:08 +0300

    feat(application): renamed TotalPages to TotalCount in SearchArticleResponse

--- Application/Features/Articles/SearchArticles/SearchArticlesResponse.cs
+++ Application/Features/Articles/SearchArticles/SearchArticlesResponse.cs
@@ -3,7 +3,7 @@ namespace Application.Features.Articles.SearchArticles;
 public record SearchArticlesResponse(
     int Page,
     int Count,
-    int TotalPages,
+    int TotalCount,
     List<SearchArticlesResponse.Element> Data)
 {
     public record Element(string Id, string ArticleTitle);
